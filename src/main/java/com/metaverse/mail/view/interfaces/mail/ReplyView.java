6a8e464f3d8fc0dca71d5a393ac968ac14bed646
Yujin Choe <120430864+choe-yujin@users.noreply.github.com>
2025-04-01 15:34:28 +0900

    Add ReplyView interface for email reply functionality

---
+++ src/main/java/com/metaverse/mail/view/interfaces/mail/ReplyView.java
@@ -0,0 +1,19 @@
+package com.metaverse.mail.view.interfaces.mail;
+
+import com.metaverse.mail.dto.mail.ReceivedEmailDto;
+
+/**
+ * 메일 답장 화면 인터페이스
+ *
+ * @author 유진
+ * @version 1.0
+ */
+public interface ReplyView {
+    
+    /**
+     * 답장 작성 화면 표시
+     *
+     * @param originalEmail 원본 이메일 정보
+     */
+    void showReplyForm(ReceivedEmailDto originalEmail);
+}
